17ac90053ebe19af13513b24d7289f361b78d963
Limeking64 <93942691+Limeking64@users.noreply.github.com>
2021-11-14 00:14:12 +0000

    Example challenge

---
+++ AdventOfCode/AdventOfCode/ExampleChallange/ExampleChallange.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.ExampleChallange
+{
+    public static class ExampleChallange
+    {
+        public static int GetAnswer(int target = 2020)
+        {
+            var entries = ReadEntriesFromFile().OrderBy(x => x).ToArray();
+
+            for (int i = 0; i < entries.Length; i++)
+            {
+                for (int k = i + 1; k < entries.Length; k++)
+                {
+                    var sum = entries[i] + entries[k];
+
+                    if (sum == target)
+                    {
+                        return entries[i] * entries[k];
+                    }
+                }                              
+            }
+
+            throw new Exception("Unable to find two numbers that add to target");
+        }
+
+        private static int[] ReadEntriesFromFile()
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory, "ExampleChallange/ExampleChallangeEntries.txt");
+            var entries = File.ReadAllLines(filePath);
+
+            return entries.Select(x => int.Parse(x)).ToArray();
+        }
+    }
+}
