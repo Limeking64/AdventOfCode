574ef487749921dc72faf45017278df770348df6
Limeking64 <93942691+Limeking64@users.noreply.github.com>
2021-12-05 22:03:49 +0000

    Day 5

---
+++ AdventOfCode/AdventOfCode/Day5/Line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Day5
+{
+    public class Line
+    {
+        public int StartingX { get; set; }
+        public int StartingY { get; set; }
+        public int EndingX { get; set; }
+        public int EndingY { get; set; }
+    }
+}
