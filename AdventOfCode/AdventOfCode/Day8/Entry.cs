436ebcd72788c4ca9f9c75707fcee40dbdfef7e9
Limeking64 <93942691+Limeking64@users.noreply.github.com>
2021-12-08 19:46:06 +0000

    Day 8

---
+++ AdventOfCode/AdventOfCode/Day8/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Day8
+{
+    public class Entry
+    {
+        public List<string> SignalPatterns { get; set; }
+
+        public List<string> OutputValues { get; set; }
+    }
+}
